c774e3be352ec12df41d8532f4983786b54ce316
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-12-20 17:46:03 +0200

    refactor: remove it in all tests

--- test/application/entities/notification/content.spec.ts
+++ test/application/entities/notification/content.spec.ts
@@ -1,16 +1,16 @@
 import { Content } from '../../../../src/application/entities/notification/content';
 
 describe('Notification Content', () => {
-  it('it should be able to create a notification content', () => {
+  it('should be able to create a notification content', () => {
     const content = new Content('You receive a request friend');
     expect(content).toBeTruthy();
   });
 
-  it('it should not be able to create a notification content with less than 5 characters', () => {
+  it('should not be able to create a notification content with less than 5 characters', () => {
     expect(() => new Content('aaa')).toThrow();
   });
 
-  it('it should not be able to create a notification content with more than 240 characters ', () => {
+  it('should not be able to create a notification content with more than 240 characters ', () => {
     expect(() => new Content('a'.repeat(241))).toThrow();
   });
 });
