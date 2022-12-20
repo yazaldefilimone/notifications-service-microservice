e7136b2117bbfe9686350b04f3670e400cb13d3f
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-12-20 17:45:07 +0200

    test: should not be able to create a notification content with more than 240 characters

--- test/application/entities/notification/content.spec.ts
+++ test/application/entities/notification/content.spec.ts
@@ -9,4 +9,8 @@ describe('Notification Content', () => {
   it('it should not be able to create a notification content with less than 5 characters', () => {
     expect(() => new Content('aaa')).toThrow();
   });
+
+  it('it should not be able to create a notification content with more than 240 characters ', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
