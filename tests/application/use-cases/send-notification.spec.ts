5b7f0ada02ae881404bca6d33d5a2118c208c222
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-12-22 21:23:27 +0200

    test: create fake `NotificationRepository` in test

--- tests/application/use-cases/send-notification.spec.ts
+++ tests/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,23 @@
+import { Notification } from '../../../src/application/entities/notification';
 import { SendNotification } from '../../../src/application/use-cases/send-notification';
+
+const notifications: Notification[] = [];
+const notificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send Notification', () => {
   it('should br able to send a  notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recipientId: 'exemple-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
