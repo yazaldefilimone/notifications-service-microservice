5aef0a59c024e9e52164f9976b55a6d410b7ab05
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-12-23 22:19:05 +0200

    feat: create databases module

---
+++ src/infrastructure/databases/database.mudule.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { NotificationRepository } from '../../application/repositories/notifications-repository';
+import { PrismaService } from './prisma.service';
+import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: NotificationRepository,
+      useClass: PrismaNotificationsRepository,
+    },
+  ],
+  exports: [NotificationRepository],
+})
+export class DatabaseModule {}
